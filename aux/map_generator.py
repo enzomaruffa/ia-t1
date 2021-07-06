78587496fe469709fde8ef9a46f5426fdc28b440
Enzo Maruffa <enzomm1999@gmail.com>
2021-07-05 23:06:32 -0300

    Adds a simple map generator

---
+++ aux/map_generator.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from random import randint
+  
+if (len(sys.argv) < 3):
+    exit("Missing arguments")
+  
+lines = int(sys.argv[1])
+columns = int(sys.argv[2])
+colors = int(sys.argv[3])
+
+os.makedirs('generated_maps')
+
+f = open(f"generated_maps/map-{lines}-{columns}-{colors}", "w")
+f.write(f"{lines} {columns} {colors}\n")
+
+for i in range(lines):
+    line = [str(randint(1, colors)) for j in range(columns)]
+    line_str = " ".join(line) + "\n"
+    f.write(line_str)
+
+f.close()
